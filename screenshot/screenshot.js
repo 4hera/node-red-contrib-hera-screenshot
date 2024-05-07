fc43e5c2cabaab388b5024facc5a78a299a67bfa
Burak Simsek <burak.simsek@4hera.com>
2024-05-07 14:01:48 +0300

    buffer fixed
    Signed-off-by: Burak Simsek <burak.simsek@4hera.com>

--- screenshot/screenshot.js
+++ screenshot/screenshot.js
@@ -6,7 +6,7 @@ module.exports = function(RED) {
     var node = this;
     this.on('input', function(msg) {
       screenshot().then((img) => {
-          msg.payload = img.toString('base64');
+          msg.payload = Buffer.from(img).toString('base64');
           node.send(msg);
       }).catch((err) => {
           node.warn(err);
